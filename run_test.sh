e5b4b2269f66d81f71308ec5ff233e8ac7d39dc5
Rohan Gupta <r42gupta@uwaterloo.ca>
2019-03-28 10:48:40 -0400

    made the script better by adding all files to time-stamped directory

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+timestamp=$(date +%H%M%S)
+mkdir -p ./tests/output/$1/$timestamp
 uw-sim --nogui -Gtest_num=$1 kirsch.uwp
 ted_to_bmp ./tests/res$1_sim.ted
+mv ./tests/res$1_sim.ted ./tests/output/$1/$timestamp/sim.ted
+mv ./tests/res$1_sim.bmp ./tests/output/$1/$timestamp/sim.bmp
+diff_ted ./tests/res$1_spec.ted ./tests/output/$1/$timestamp/sim.ted > ./tests/output/$1/$timestamp/diff.ted
+diff_ted_to_bmp ./tests/res$1_spec.ted ./tests/output/$1/$timestamp/sim.ted ./tests/output/$1/$timestamp/diff.bmp
